26bf7c10850e0c2f03f0f5ed1cefd3d99dfbceb0
Jo Preihs <jo_preihs@hotmail.com>
2019-04-19 21:34:05 +0100

    React Todo App - In progress

--- src/App.js
+++ src/App.js
@@ -4,18 +4,35 @@ import Nav from './components/Nav';
 import AddItem from "./components/AddItem";
 import ListItem from "./components/ListItem";
 
+const todoListItems = [
+  "Do hoovering",
+  "Buy Easter Eggs",
+  "Do shopping",
+  "Do cooking"
+];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav text="Welcome to my app" />
-        <Nav text="About this app" />
-        <Nav text="Contact Us" />
+        <Nav text="Welcome to the new way to organise your life" />
+        <div id="subHeader">
+          <h4>Never forget another task again with this simple, effective life organiser</h4>
+        </div>
+        <div id="div3">
+        <span className="getStarted">
+            <h5>Let's get started!</h5>
+          </span>
+        </div>
+        <div id="div4">
+        <h5>Just jot down your tasks and the date they need to be completed by below:</h5>
+        </div>
         <AddItem />
         <h4>Here is a list of things I need to do</h4>
         <ul>
-          <ListItem task="Do hoovering"/>
-          <ListItem task="Buy Easter Eggs"/>
+          {todoListItems.map(function (item) {
+            return <ListItem task={item} />
+          })}
         </ul>
       </div>
 
--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import './AddItem.css';
 
 class AddItem extends Component {
-render() {
-    return (
-        <div>
-            <form>
-                <label>Your new todo:</label>
-                <input type="text" />
-                <button type="button">Add</button>
-            </form>
-        </div>
-    );
-}
+    render() {
+        return (
+            <div id="AddTask">
+                <form>
+                    <label><h6>Task 1</h6></label>
+                    <input type="text" />
+                    <button type="button">Add</button>
+                    <label><h6>Deadline</h6></label>
+                    <input type="date" />
+                    <label><h6>Add to my list</h6></label>
+                    <button type="Submit">Add</button>
+                </form>
+            </div>
+        );
+    }
 }
 export default AddItem;
