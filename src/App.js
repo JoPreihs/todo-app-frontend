215e2ca1a647e42fc60b26214332428c1292ad09
Jo Preihs <jo_preihs@hotmail.com>
2019-04-22 15:52:32 +0100

    React Todo App - In progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
-import Nav from './components/Nav';
+import "./App.css";
+import Nav from "./components/Nav";
 import AddItem from "./components/AddItem";
 import ListItem from "./components/ListItem";
 
@@ -25,10 +25,14 @@ class App extends Component {
           </span>
         </div>
         <div id="div4">
-          <h5>Just jot down your tasks and the date they need to be completed by below:</h5>
+          <span className="getStarted">
+            <h5>Just jot down your tasks and the date they need to be completed by below:</h5>
+          </span>
         </div>
         <AddItem />
-        <h4>Current list of tasks:</h4>
+        <div id="currentListTitle">
+          <h5>Current list of tasks:</h5>
+        </div>
         <ul>
           {todoListItems.map(function (item) {
             return <ListItem task={item} />
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+import './ListItem.css';
 
 class ListItem extends Component {
     render() {
         return (
+            <div id="currentList">
 
-            <li>
-                {this.props.task}
-                <button>Done</button>
-                <button>Delete</button>
+                <li>
+                    {this.props.task}
+                    <button>Done</button>
+                    <button>Delete</button>
 
-            </li>
-
+                </li>
+            </div>
         );
     }
 }
